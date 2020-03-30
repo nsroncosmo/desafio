90b4adc8134eb19e03fe16a791938ceb1fa777af
nsroncosmo <ronaldo.silva@cbyk.com.br>
2020-03-30 10:45:10 -0300

    Melhoria do layout

---
+++ client/src/components/Botoneira.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+export default function Botoneira() {
+	const estilo = {"float": "left","marginRight": "33px" };
+
+	return (
+
+		<div >
+
+			<div style ={estilo}>
+				<h4>Dimensões</h4>
+				<div>
+					<p>Altura: 10</p>
+					<p>Largura: 10</p>
+				</div>
+			</div>
+			<div style ={estilo}>
+				<h4>Elementos</h4>
+				<div>
+					<p>Aeroportos: 3</p>
+					<p>Núvens: 4</p>
+				</div>
+			</div>
+			<div style ={estilo}>
+				<h4>Aeroporto</h4>
+				<h5>Mantos DuBom</h5>
+			</div>
+
+			<div style={{"clear": "both"}}></div>
+		</div>
+
+)
+
+}
--- client/src/components/Controller.js
+++ client/src/components/Controller.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
+import Botoneira from '../components/Botoneira';
+import Fundo from '../components/Fundo';
 import Map from './Map';
 //import { callAPI } from '../utils/utils';
 //const { callAPI, guid  } = require('../utils/utils');
@@ -7,6 +9,21 @@ import Map from './Map';
 export default function Controller() {
 	const [rows,setRows] = useState(10);
 	const [cols,setCols] = useState(10);
+	const [dados,genDados] = useState({
+		dimensions: {
+			width: 10,
+			height: 10,
+		},
+		Elements:{
+			Airports: 3,
+			Clouds: 4,
+		},
+		Info:{
+			FDay: "?",
+			LDay: "?",
+			Msg: "Clique em [botão] para iniciar os cálculos",
+		},
+	});
 
 //	function incCols() { if (cols<50) setCols(cols + 1) }
 //	function decCols() { if (cols>10) setCols(cols - 1) }
@@ -14,16 +31,17 @@ export default function Controller() {
 //	callAPI();
 
 	return (
-		<>
-		<Fragment>
-			Topo
-			<h3>Linhas: {rows} / Colunas {cols}</h3>
-		</Fragment>
-		<Fragment>
-			<Map />
-		</Fragment>
-		<Fragment>Fundo</Fragment>
-		</>
+		<div>
+			<div>
+				<Botoneira />
+			</div>
+			<div>
+				<Map />
+			</div>
+			<div>
+				<Fundo/>
+			</div>
+		</div>
 	)
 
 }
