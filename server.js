14e69596e422d3f840e3b97f56705e9a25b5c398
nsroncosmo <ronaldo.silva@cbyk.com.br>
2020-04-05 21:54:19 -0300

    EndPoints voltarm a funcionar... Saturacao minha

--- server.js
+++ server.js
@@ -1,47 +1,54 @@
-const express       = require( 'express' );
-const bodyParser    = require( 'body-parser' );
-const { createMap } = require( './utils/utils' );
-//const kraken        = require( 'kraken-js' );
+  const express     = require( 'express' );
+  const bodyParser  = require( 'body-parser' );
+  const { doDaMap } = require( './utils/utils' );
+//const kraken      = require( 'kraken-js' );
 
-const app  = express();
-const port = process.env.PORT || 8088;
+  const server      = express();
+  const port        = process.env.PORT || 8088;
 
-app.use( bodyParser.urlencoded({ extended: true }) );
+server.use( bodyParser.urlencoded({ extended: true }) );
 
-let mapaInit;
+//let mapaInit;
 
 //var options = {
 //  onconfig: (config,next) => next(null,config)
 //}
-//app.use( kraken(options) );
+//server.use( kraken(options) );
 
-app.get('/api/test', (req, res) => {
-  res.send({
-     express: 'To funfundo',
-     req: req,
-     });
+// curl -X GET localhost:3000/api/test
+server.get( '/api/test', (req, res) => {
+  res.send({ message: 'To funfundo' });
 });
 
-app.post('/api/map/', (req, res) => {
-  const {w: cols, h: rows, a: ports, c: clouds} = req.body;
+
+// curl -X POST localhost:3000/api/map -d "w=10&h=10&a=3&c=4"
+server.post( '/api/map', (req, res) => {
+  const { w: cols, h: rows, a: ports, c: clouds } = req.body;
+
+  res.json( createMap( cols, rows, ports, clouds ) );
+});
+
+/*
+// curl -X POST localhost:3000/api/map -d "w=10&h=10&a=3&c=4"
+server.post( '/api/map', (req, res) => {
+  const { w: cols, h: rows, a: ports, c: clouds } = req.body;
 
   mapaInit = createMap( cols, rows, ports, clouds );
 
-  console.log('%c Mapping', 'color: green; font-weight: bold');
-  console.table({cols, rows, sat});
+  //console.log('%c Mservering', 'color: green; font-weight: bold');
+  console.table( { cols, rows, ports, clouds } );
 
   res.json( mapaInit );
-  console.table(mapaInit);
 });
 
-app.post('/api/calc', (req, res) => {
+server.post( '/api/calc', (req, res) => {
   const {d: day} = req.body;
   const mapaRes  = updateMap( mapaInit, day );
 
   console.log(`%c After ${day} days}`, 'color: green; font-weight: bold');
-//  console.table(mapaRes);
 
   res.json( mapaRes );
 });
+*/
 
-app.listen(port, () => console.log( `Servidor ativo na porta ${port}`) );
+server.listen( port, () => console.log( `Servidor ativo na porta ${port}`) );
--- utils/utils.js
+++ utils/utils.js
@@ -3,73 +3,114 @@ const AIRPORT            = 1;
 const CLOUD_OVER_TERRAIN = 2;
 //const CLOUD_OVER_AIRPORT = 3;
 
-let mapInit;
+let mapCalc;
 
-function rndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+function rndInteger( min, max ) {
+return Math.floor( Math.random() * (max - min) ) + min;
 }
 
-function createArray( width, height ){
-  let w = width + 2, h = height + 2;
+function createArray( width, height ) {
+let w = parseInt(width) + 2, h = parseInt(height) + 2;
 
-  mapInit = null;
-  mapInit = new Array(w);
+mapCalc = null;
+mapCalc = new Array(w);
 
-  for (x = 0; x < w; x++) {
-    mapInit[x] = new Array(h);
+for (x = 0; x < w; x++) {
+  mapCalc[x] = new Array(h);
 
-    for (y = 0; y < h; y++)
-      mapInit[x][y] = TERRAIN;
-  }
-  //console.log(`width [${width}] / w [${w}] / map.length ${mapInit.length}`)
-  //console.log(`height [${height}] / h [${h}] / map[0].length ${mapInit[0].length}`)
+  for (y = 0; y < h; y++)
+    mapCalc[x][y] = TERRAIN;
+}
+//console.log(`width [${width}] / w [${w}] / map.length ${mapCalc.length}`)
+//console.log(`height [${height}] / h [${h}] / map[0].length ${mapCalc[0].length}`)
 }
 
-function populateMap( nAirports, nClouds ){
-  let count, x, y;
-  const width  = mapInit.length - 1;
-  const height = mapInit[0].length - 1;
+function populateMap( nAirports, nClouds ) {
+let count, x, y;
+const width  = mapCalc.length    - 1;
+const height = mapCalc[0].length - 1;
 
-  count = nAirports;
-  while(count>0){
-    x = rndInteger( 1, width  );
-    y = rndInteger( 1, height );
+count = nAirports;
+while ( count > 0 ) {
+  x = rndInteger( 1, width  );
+  y = rndInteger( 1, height );
 
-    if (mapInit[x][y] === TERRAIN){
-        mapInit[x][y] = AIRPORT;
-      count--;
-    }
+  if ( mapCalc[x][y] === TERRAIN ) {
+       mapCalc[x][y] = AIRPORT;
+    count--;
   }
+}
 
-  count = nClouds;
-  while(count>0){
-    x = rndInteger( 1, width  );
-    y = rndInteger( 1, height );
+count = nClouds;
+while ( count > 0 ) {
+  x = rndInteger( 1, width  );
+  y = rndInteger( 1, height );
 
-    if (mapInit[x][y] === TERRAIN){
+  if ( mapCalc[x][y] === TERRAIN ) {
 //console.log(`${x}x${y} = ${CLOUD_OVER_TERRAIN}`);
 
-        mapInit[x][y] = CLOUD_OVER_TERRAIN;
-      count--;
-    }
+       mapCalc[x][y] = CLOUD_OVER_TERRAIN;
+    count--;
   }
+}
 
 }
 
 function createMap( width, height, nAirports, nClouds ) {
-  { // Validações
-    width     = width     < 10 || undefined == width     ? 10 : width    ;
-    height    = height    < 10 || undefined == height    ? 10 : height   ;
-    nAirports = nAirports <  3 || undefined == nAirports ?  3 : nAirports;
-    nClouds   = nClouds   <  4 || undefined == nClouds   ?  4 : nClouds  ;
-  }
-
-  createArray( width, height );
-
-  populateMap( nAirports, nClouds );
-
-	return mapInit;
+{ // Validações
+  width     = width     < 10 || undefined == width     ? 10 : width    ;
+  height    = height    < 10 || undefined == height    ? 10 : height   ;
+  nAirports = nAirports <  3 || undefined == nAirports ?  3 : nAirports;
+  nClouds   = nClouds   <  4 || undefined == nClouds   ?  4 : nClouds  ;
 }
+
+//console.log("Dentro da createMap");
+console.table( { width, height, nAirports, nClouds } )
+
+width = 4; height = 4;
+
+createArray( width, height );
+
+//console.log(`mapCalc.length [${mapCalc.length}]`);
+//console.log(`mapCalc[0].length [${mapCalc[0].length}]`);
+
+//console.log("Mapa vazio");
+//console.table( mapCalc )
+
+populateMap( nAirports, nClouds );
+
+//console.log("Mapa populado");
+//console.table( mapCalc )
+
+//return mapCalc;
+}
+
+function updateMap( map, day ) {
+let x, y;
+let mapRes = [...mapCalc], mapInit = [...mapCalc];
+const width  = mapRes.length - 2;
+const height = mapRes[0].length - 2;
+
+/*
+const TERRAIN            = 0;
+const AIRPORT            = 1;
+const CLOUD_OVER_TERRAIN = 2;
+const CLOUD_OVER_AIRPORT = 3; 
+*/
+
+for ( x = 1; x < width; x++ ) {
+  for ( y = 1; y < height; y++ ) {
+   if( mapInit[x][y] >= CLOUD_OVER_TERRAIN ){
+      mapRes[x+1][y] = mapRes[x+1][y] <= AIRPORT ? mapRes[x+1][y] += CLOUD_OVER_TERRAIN : mapRes[x+1][y];
+      mapRes[x-1][y] = mapRes[x-1][y] <= AIRPORT ? mapRes[x-1][y] += CLOUD_OVER_TERRAIN : mapRes[x-1][y];
+      mapRes[x][y-1] = mapRes[x][y-1] <= AIRPORT ? mapRes[x][y-1] += CLOUD_OVER_TERRAIN : mapRes[x][y-1];
+      mapRes[x][y+1] = mapRes[x][y+1] <= AIRPORT ? mapRes[x][y+1] += CLOUD_OVER_TERRAIN : mapRes[x][y+1];
+    }
+  }
+}
+return mapRes;
+}
+
 
 module.exports = {
 	createMap,
