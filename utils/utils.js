d2e213b808e98d8b97391333265355725f00bdd2
nsroncosmo <ronaldo.silva@cbyk.com.br>
2020-04-08 06:40:01 -0300

    melhorias no backend

--- utils/utils.js
+++ utils/utils.js
@@ -3,7 +3,7 @@ const AIRPORT            = 1;
 const CLOUD_OVER_TERRAIN = 2;
 //const CLOUD_OVER_AIRPORT = 3;
 
-let mapCalc;
+let mapCalc, mapAirport = [], coordAirport = [], calcAirport, totAirport;
 
 function rndInteger( min, max ) {
 return Math.floor( Math.random() * (max - min) ) + min;
@@ -26,20 +26,27 @@ for (x = 0; x < w; x++) {
 }
 
 function populateMap( nAirports, nClouds ) {
-let count, x, y;
+let count, x, y, map;
 const width  = mapCalc.length    - 1;
 const height = mapCalc[0].length - 1;
 
-count = nAirports;
+totAirport = nAirports
+count      = nAirports;
 while ( count > 0 ) {
   x = rndInteger( 1, width  );
   y = rndInteger( 1, height );
 
   if ( mapCalc[x][y] === TERRAIN ) {
        mapCalc[x][y] = AIRPORT;
-    count--;
+
+    map        = {x,y};
+    coordAirport = [ ...coordAirport, map ];
+    count      --;
   }
 }
+
+console.log("coordAirport")
+console.table(coordAirport);
 
 count = nClouds;
 while ( count > 0 ) {
@@ -47,7 +54,7 @@ while ( count > 0 ) {
   y = rndInteger( 1, height );
 
   if ( mapCalc[x][y] === TERRAIN ) {
-//console.log(`${x}x${y} = ${CLOUD_OVER_TERRAIN}`);
+  //console.log(`${x}x${y} = ${CLOUD_OVER_TERRAIN}`);
 
        mapCalc[x][y] = CLOUD_OVER_TERRAIN;
     count--;
@@ -85,6 +92,10 @@ populateMap( nAirports, nClouds );
 //return mapCalc;
 }
 
+function spreadCloud(x,y) {
+
+}
+
 function updateMap( map, day ) {
 let x, y;
 let mapRes = [...mapCalc], mapInit = [...mapCalc];
@@ -98,9 +109,11 @@ const CLOUD_OVER_TERRAIN = 2;
 const CLOUD_OVER_AIRPORT = 3; 
 */
 
+calcAirport = 0
+
 for ( x = 1; x < width; x++ ) {
   for ( y = 1; y < height; y++ ) {
-   if( mapInit[x][y] >= CLOUD_OVER_TERRAIN ){
+   if( mapInit[x][y] >= CLOUD_OVER_TERRAIN ) {
       mapRes[x+1][y] = mapRes[x+1][y] <= AIRPORT ? mapRes[x+1][y] += CLOUD_OVER_TERRAIN : mapRes[x+1][y];
       mapRes[x-1][y] = mapRes[x-1][y] <= AIRPORT ? mapRes[x-1][y] += CLOUD_OVER_TERRAIN : mapRes[x-1][y];
       mapRes[x][y-1] = mapRes[x][y-1] <= AIRPORT ? mapRes[x][y-1] += CLOUD_OVER_TERRAIN : mapRes[x][y-1];
@@ -108,10 +121,36 @@ for ( x = 1; x < width; x++ ) {
     }
   }
 }
-return mapRes;
+
+mapAirport = [ {mapRes}, ...mapAirport ];
+
+console.log("mapRes --------------");
+console.table(mapRes);
+
+coordAirport.forEach( coord => {
+    if ( mapRes[coord.x][coord.y] === AIRPORT ) calcAirport++;
+} );
+
+return (calcAirport===totAirport); //mapRes;
 }
 
+function calcMap( width, height, nAirports, nClouds ) {
 
-module.exports = {
-	createMap,
-	};
+createMap( width, height, nAirports, nClouds );
+
+while ( updateMap() ) {}
+
+console.log("mapAirport --------------");
+console.table(mapAirport);
+
+//  console.log("init" ,mapCalc);
+//  console.log("dia 1",updateMap(1));
+}
+
+calcMap()
+
+//Module.exports = {
+//	doDaMap,
+//	};
+
+
